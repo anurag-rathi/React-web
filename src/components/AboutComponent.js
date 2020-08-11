ecd5879e1c83943c6f80e2087f3888e97e7fd9d3
anurag-rathi <anuragrathi66@gmail.com>
2020-08-11 18:18:04 +0530

    Assignment  4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,35 +1,57 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+import { FadeTransform , Fade, Stagger } from 'react-animation-components';
 
-function RenderLeader({leader}){
-   return(
-            <div key={leader.id} className="col-12 mt-5" >
-                <Media tag="li">
-                    <Media left middle>
-                        <Media object src={leader.image} alt={leader.name} />
-                    </Media>
-                    <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                    </Media>
-                </Media>
-            </div>
-     );
+function RenderLeader({leaders ,isLoading, errMess}){
+   const leaders_list = leaders.map((leader) => {
+        return(
+            
+                <div key={leader.id} className="col-12 mt-5" >
+                    <Fade in>
+                        <Media tag="li">
+                            <Media left middle>
+                                <Media object src={baseUrl + leader.image} alt={leader.name} />
+                            </Media>
+                            <Media body className="ml-5">
+                            <Media heading>{leader.name}</Media>
+                            <p>{leader.designation}</p>
+                            <p>{leader.description}</p>
+                            </Media>
+                        </Media>
+                    </Fade>
+                </div>
+        );
+   })
+
+   if(isLoading){
+       return(
+           <Loading />
+       );
+   }
+   else if(errMess){
+       return(
+           <h4>{errMess}</h4>
+       );
+   }
+   else {
+       return(
+           <Media list>
+               <Stagger in>
+                 {leaders_list}
+               </Stagger>
+           </Media>
+       );
+   }
+  
 
 }
+ 
 
 function About(props) {
-
-    const leaders = props.leaders.map( (leader) => {
-
-        return(
-            <RenderLeader leader= {leader} />
-        );
-
-    });
-
+    
     return(
         <div className="container">
             <div className="row">
@@ -85,9 +107,10 @@ function About(props) {
                     <h2>Corporate Leadership</h2>
                 </div>
                 <div className="col-12">
-                    <Media list>
-                         {leaders}
-                    </Media>
+                    <RenderLeader leaders= {props.leaders}
+                        isLoading= {props.isLoading}
+                        errMess= {props.errMess}
+                    />
                 </div>
             </div>
         </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators'
+import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders, postFeedback } from '../redux/ActionCreators'
 import { actions } from 'react-redux-form';
 import { TransitionGroup , CSSTransition } from 'react-transition-group'
 
@@ -26,7 +26,9 @@ const mapDispatchToProps = dispatch => ({
    fetchDishes : () =>{ dispatch(fetchDishes()) },
    resetFeedbackForm : () => { dispatch(actions.reset('feedback')) },
    fetchPromos : () => { dispatch(fetchPromos()) },
-   fetchComments : () => { dispatch(fetchComments()) }
+   fetchComments : () => { dispatch(fetchComments()) },
+   fetchLeaders : () => { dispatch(fetchLeaders()) },
+   postFeedback : (firstname,lastname,telnum,email,agree,contactType,message) => dispatch(postFeedback(firstname,lastname,telnum,email,agree,contactType,message))
 })
 
 
@@ -41,6 +43,7 @@ class Main extends Component {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
+    this.props.fetchLeaders();
   }
 
     render() { 
@@ -53,7 +56,9 @@ class Main extends Component {
                 promotion = {this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
                 promoLoading = {this.props.promotions.isLoading}
                 promoErrMess = {this.props.promotions.errMess}
-                leader = {this.props.leaders.filter((leader) => leader.featured)[0]}
+                leader = {this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+                leaderLoading = {this.props.leaders.isLoading}
+                leaderErrMess = {this.props.leaders.errMess}
            />
          );
       }
@@ -79,8 +84,8 @@ class Main extends Component {
                       <Route path="/home" component= {Homepage} />
                       <Route exact path ="/menu" component= { () => <Menu dishes = {this.props.dishes} />} />
                       <Route path="/menu/:dishId" component = {DishWithId} />
-                      <Route path = '/contactus' component= {() => <Contact resetFeedbackForm = {this.props.resetFeedbackForm} /> } />
-                      <Route path = '/aboutus' component= { () => <About leaders = {this.props.leaders} />} />
+                      <Route path = '/contactus' component= {() => <Contact resetFeedbackForm = {this.props.resetFeedbackForm} postFeedback = {this.props.postFeedback} /> } />
+                      <Route path = '/aboutus' component= { () => <About leaders = {this.props.leaders.leaders} isLoading={this.props.leaders.isLoading} errMess = {this.props.leaders.errMess} />} />
                       <Redirect to= "/home" />
                   </Switch>
                 </CSSTransition>
